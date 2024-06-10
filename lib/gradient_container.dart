5fbdc01fe1a1bafe4d3113b0536a871f46b59926
devMotcho <code.mmd2@gmail.com>
2024-06-10 22:43:10 +0100

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/styled_text.dart';
+
+// Custom Widget
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/gradient_container.dart';
+
+// https://m3.material.io/
+// https://docs.flutter.dev/ui/widgets
+void main() {
+  // Custome funcion body
+  // MaterialAPP -> Main widget
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  ); //create widget
+}
+
+
