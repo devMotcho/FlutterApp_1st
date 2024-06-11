058c24109b5a19d236ed8f17c66c8a939307c36d
devMotcho <code.mmd2@gmail.com>
2024-06-11 22:21:03 +0100

    Variables and reuse code

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(Colors.deepPurple, Color.fromARGB(255, 161, 126, 223)),
       ),
     ),
   ); //create widget
